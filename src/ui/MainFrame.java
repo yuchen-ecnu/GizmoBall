2d93241883f9e3dee4b4c7937592ed3db0e8eb85
Chenyu <920901835@qq.com>
2017-11-21 21:48:58 +0800

    修正图片错误

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -63,34 +63,34 @@ public class MainFrame extends JFrame {
 		setSize(Constant.WINDOW_MAIN_WIDTH, Constant.WINDOW_MAIN_HEIGHT);
 		Dimension midDimension = ScreenUtils.getScreenMidDimension(this);
 		setLocation(midDimension.width, midDimension.height);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem m_new = new JMenuItem("New");
 		mnFile.add(m_new);
-		
+
 		JMenuItem m_open = new JMenuItem("Open");
 		mnFile.add(m_open);
-		
+
 		JMenuItem m_save = new JMenuItem("Save");
 		mnFile.add(m_save);
-		
+
 		JMenuItem m_exit = new JMenuItem("Exit");
 		mnFile.add(m_exit);
-		
+
 		JMenu mnSetting = new JMenu("Setting");
 		menuBar.add(mnSetting);
-		
+
 		JCheckBoxMenuItem m_checkCollision = new JCheckBoxMenuItem("Collision Effect");
 		mnSetting.add(m_checkCollision);
-		
+
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
-		
+
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnHelp.add(mntmAbout);
 		contentPane = new JPanel();
@@ -98,133 +98,145 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		BoardPanel boardPanel = new BoardPanel();
 		contentPane.add(boardPanel);
-		
+
 		JPanel operationPanel = new JPanel();
 		operationPanel.setBackground(Color.WHITE);
 		operationPanel.setBounds(633, 10, 191, 610);
 		contentPane.add(operationPanel);
 		operationPanel.setLayout(null);
-		
-		JPanel basicCompPanel = new JPanel();
-		basicCompPanel.setBackground(Color.WHITE);
-		basicCompPanel.setBorder(new TitledBorder(null, "Basic Component", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		basicCompPanel.setBounds(10, 202, 160, 182);
-		operationPanel.add(basicCompPanel);
-		basicCompPanel.setLayout(null);
-		
+
+		JPanel advanceCompPanel = new JPanel();
+		advanceCompPanel.setBackground(Color.WHITE);
+		advanceCompPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Advance Component", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		advanceCompPanel.setBounds(10, 202, 160, 182);
+		operationPanel.add(advanceCompPanel);
+		advanceCompPanel.setLayout(null);
+
 		JButton button_3 = new JButton("");
 		button_3.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_3.setContentAreaFilled(false);
 		button_3.setOpaque(false);
 		button_3.setBackground(Color.WHITE);
 		button_3.setBounds(30, 27, 40, 40);
-		basicCompPanel.add(button_3);
-		
+		button_3.setIcon(ImageUtils.getImage("stick_square.png", 30, 30));
+		advanceCompPanel.add(button_3);
+
 		JButton button_4 = new JButton("");
 		button_4.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_4.setContentAreaFilled(false);
 		button_4.setOpaque(false);
 		button_4.setBackground(Color.WHITE);
 		button_4.setBounds(94, 27, 40, 40);
-		basicCompPanel.add(button_4);
-		
+		button_4.setIcon(ImageUtils.getImage("stick.png", 30, 30));
+		advanceCompPanel.add(button_4);
+
 		JButton button_5 = new JButton("");
+		button_5.setToolTipText("left board");
 		button_5.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_5.setContentAreaFilled(false);
 		button_5.setOpaque(false);
 		button_5.setBackground(Color.WHITE);
 		button_5.setBounds(30, 77, 40, 40);
-		basicCompPanel.add(button_5);
-		
+		button_5.setIcon(ImageUtils.getImage("left_flipper.png", 30, 30));
+		advanceCompPanel.add(button_5);
+
 		JButton button_6 = new JButton("");
 		button_6.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_6.setContentAreaFilled(false);
 		button_6.setOpaque(false);
 		button_6.setBackground(Color.WHITE);
 		button_6.setBounds(94, 77, 40, 40);
-		basicCompPanel.add(button_6);
-		
+		button_6.setIcon(ImageUtils.getImage("right_flipper.png", 30, 30));
+		advanceCompPanel.add(button_6);
+
 		JButton button_9 = new JButton("");
+		button_9.setToolTipText("Absorber,used to recycle balls");
 		button_9.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_9.setContentAreaFilled(false);
 		button_9.setOpaque(false);
 		button_9.setBackground(Color.WHITE);
 		button_9.setBounds(30, 132, 40, 40);
-		basicCompPanel.add(button_9);
-		
+		button_9.setIcon(ImageUtils.getImage("absorber.png", 30, 30));
+		advanceCompPanel.add(button_9);
+
 		JButton button_10 = new JButton("");
 		button_10.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_10.setContentAreaFilled(false);
 		button_10.setOpaque(false);
 		button_10.setBackground(Color.WHITE);
 		button_10.setBounds(94, 132, 40, 40);
-		basicCompPanel.add(button_10);
-		
-		JPanel AdvanCompPanel = new JPanel();
-		AdvanCompPanel.setBorder(new TitledBorder(null, "Basic Component", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		AdvanCompPanel.setBackground(Color.WHITE);
-		AdvanCompPanel.setBounds(10, 10, 160, 182);
-		operationPanel.add(AdvanCompPanel);
-		AdvanCompPanel.setLayout(null);
-		
+		advanceCompPanel.add(button_10);
+
+		JPanel basicCompoPanel = new JPanel();
+		basicCompoPanel.setBorder(new TitledBorder(null, "Basic Component", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		basicCompoPanel.setBackground(Color.WHITE);
+		basicCompoPanel.setBounds(10, 10, 160, 182);
+		operationPanel.add(basicCompoPanel);
+		basicCompoPanel.setLayout(null);
+
 		JButton btnTrian = new JButton("");
 		btnTrian.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnTrian.setContentAreaFilled(false);
 		btnTrian.setOpaque(false);
 		btnTrian.setBackground(Color.WHITE);
 		btnTrian.setBounds(28, 32, 40, 40);
-		AdvanCompPanel.add(btnTrian);
-		
+		btnTrian.setIcon(ImageUtils.getImage("circle.png", 30, 30));
+		basicCompoPanel.add(btnTrian);
+
 		JButton button = new JButton("");
 		button.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button.setContentAreaFilled(false);
 		button.setOpaque(false);
 		button.setBackground(Color.WHITE);
 		button.setBounds(92, 32, 40, 40);
-		AdvanCompPanel.add(button);
-		
+		button.setIcon(ImageUtils.getImage("triangle.png", 30, 30));
+		basicCompoPanel.add(button);
+
 		JButton button_1 = new JButton("");
 		button_1.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_1.setContentAreaFilled(false);
 		button_1.setOpaque(false);
 		button_1.setBackground(Color.WHITE);
 		button_1.setBounds(92, 82, 40, 40);
-		AdvanCompPanel.add(button_1);
-		
+		button_1.setIcon(ImageUtils.getImage("trapezoid.png", 30, 30));
+		basicCompoPanel.add(button_1);
+
 		JButton button_2 = new JButton("");
 		button_2.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_2.setContentAreaFilled(false);
 		button_2.setOpaque(false);
 		button_2.setBackground(Color.WHITE);
 		button_2.setBounds(28, 82, 40, 40);
-		AdvanCompPanel.add(button_2);
-		
+		button_2.setIcon(ImageUtils.getImage("square.png", 30, 30));
+		basicCompoPanel.add(button_2);
+
 		JButton button_7 = new JButton("");
 		button_7.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_7.setContentAreaFilled(false);
 		button_7.setOpaque(false);
 		button_7.setBackground(Color.WHITE);
 		button_7.setBounds(28, 132, 40, 40);
-		AdvanCompPanel.add(button_7);
-		
+		button_7.setIcon(ImageUtils.getImage("ball.png", 30, 30));
+		basicCompoPanel.add(button_7);
+
 		JButton button_8 = new JButton("");
 		button_8.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		button_8.setContentAreaFilled(false);
 		button_8.setOpaque(false);
 		button_8.setBackground(Color.WHITE);
 		button_8.setBounds(92, 132, 40, 40);
-		AdvanCompPanel.add(button_8);
-		
+		basicCompoPanel.add(button_8);
+
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Operation", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(10, 395, 160, 205);
 		operationPanel.add(panel);
-		
+
 		JButton btnRotation = new JButton();
 		btnRotation.setOpaque(false);
 		btnRotation.setContentAreaFilled(false);
@@ -233,7 +245,7 @@ public class MainFrame extends JFrame {
 		btnRotation.setBounds(30, 30, 40, 40);
 		btnRotation.setIcon(ImageUtils.getImage("rotation.png", 30, 30));
 		panel.add(btnRotation);
-		
+
 		JButton btnDel = new JButton();
 		btnDel.setOpaque(false);
 		btnDel.setContentAreaFilled(false);
@@ -242,7 +254,7 @@ public class MainFrame extends JFrame {
 		btnDel.setBounds(90, 30, 40, 40);
 		btnDel.setIcon(ImageUtils.getImage("delete.png", 30, 30));
 		panel.add(btnDel);
-		
+
 		JButton btnPlay = new JButton("Play");
 		btnPlay.setOpaque(false);
 		btnPlay.setContentAreaFilled(false);
@@ -251,13 +263,12 @@ public class MainFrame extends JFrame {
 		btnPlay.setBounds(30, 80, 100, 30);
 		btnPlay.setIcon(ImageUtils.getImage("start.png", 20, 20));
 		panel.add(btnPlay);
-		
+
 		JButton btnDonate = new JButton("Donate");
 		btnDonate.setBounds(30, 160, 100, 28);
 		btnDonate.setIcon(ImageUtils.getImage("donate.png", 20, 20));
 		panel.add(btnDonate);
 		btnDonate.addActionListener(new ActionListener() {
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				DonateDialog dialog = new DonateDialog(MainFrame.this);
 				dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -268,7 +279,7 @@ public class MainFrame extends JFrame {
 		btnDonate.setContentAreaFilled(false);
 		btnDonate.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnDonate.setBackground(Color.WHITE);
-		
+
 		JButton btnPause = new JButton("Pause");
 		btnPause.setOpaque(false);
 		btnPause.setContentAreaFilled(false);
