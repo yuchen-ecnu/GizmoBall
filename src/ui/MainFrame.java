b1ef79962f12ca462e4c8e43ae24d7135b300702
Jack Chen <920901835@qq.com>
2017-11-22 17:43:01 +0800

    添加sizeUI

--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -6,23 +6,15 @@ import utils.ScreenUtils;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JDialog;
 
 import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -96,6 +88,7 @@ public class MainFrame extends JFrame {
 		contentPane.setLayout(null);
 
 		BoardPanel boardPanel = new BoardPanel();
+		boardPanel.setLocation(10, 10);
 		contentPane.add(boardPanel);
 
 		JPanel operationPanel = new JPanel();
@@ -107,7 +100,7 @@ public class MainFrame extends JFrame {
 		JPanel advanceCompPanel = new JPanel();
 		advanceCompPanel.setBackground(Color.WHITE);
 		advanceCompPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Advance Component", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		advanceCompPanel.setBounds(10, 202, 160, 182);
+		advanceCompPanel.setBounds(10, 184, 160, 171);
 		operationPanel.add(advanceCompPanel);
 		advanceCompPanel.setLayout(null);
 
@@ -116,7 +109,7 @@ public class MainFrame extends JFrame {
 		button3.setContentAreaFilled(false);
 		button3.setOpaque(false);
 		button3.setBackground(Color.WHITE);
-		button3.setBounds(30, 27, 40, 40);
+		button3.setBounds(31, 21, 40, 40);
 		button3.setIcon(ImageUtils.getImage("stick_square.png", 30, 30));
 		advanceCompPanel.add(button3);
 
@@ -125,7 +118,7 @@ public class MainFrame extends JFrame {
 		button_4.setContentAreaFilled(false);
 		button_4.setOpaque(false);
 		button_4.setBackground(Color.WHITE);
-		button_4.setBounds(94, 27, 40, 40);
+		button_4.setBounds(95, 21, 40, 40);
 		button_4.setIcon(ImageUtils.getImage("stick.png", 30, 30));
 		advanceCompPanel.add(button_4);
 
@@ -135,7 +128,7 @@ public class MainFrame extends JFrame {
 		button_5.setContentAreaFilled(false);
 		button_5.setOpaque(false);
 		button_5.setBackground(Color.WHITE);
-		button_5.setBounds(30, 77, 40, 40);
+		button_5.setBounds(31, 71, 40, 40);
 		button_5.setIcon(ImageUtils.getImage("left_flipper.png", 30, 30));
 		advanceCompPanel.add(button_5);
 
@@ -144,7 +137,7 @@ public class MainFrame extends JFrame {
 		button_6.setContentAreaFilled(false);
 		button_6.setOpaque(false);
 		button_6.setBackground(Color.WHITE);
-		button_6.setBounds(94, 77, 40, 40);
+		button_6.setBounds(95, 71, 40, 40);
 		button_6.setIcon(ImageUtils.getImage("right_flipper.png", 30, 30));
 		advanceCompPanel.add(button_6);
 
@@ -154,7 +147,7 @@ public class MainFrame extends JFrame {
 		button_9.setContentAreaFilled(false);
 		button_9.setOpaque(false);
 		button_9.setBackground(Color.WHITE);
-		button_9.setBounds(30, 132, 40, 40);
+		button_9.setBounds(31, 121, 40, 40);
 		button_9.setIcon(ImageUtils.getImage("absorber.png", 30, 30));
 		advanceCompPanel.add(button_9);
 
@@ -163,13 +156,13 @@ public class MainFrame extends JFrame {
 		button_10.setContentAreaFilled(false);
 		button_10.setOpaque(false);
 		button_10.setBackground(Color.WHITE);
-		button_10.setBounds(94, 132, 40, 40);
+		button_10.setBounds(95, 121, 40, 40);
 		advanceCompPanel.add(button_10);
 
 		JPanel basicCompoPanel = new JPanel();
 		basicCompoPanel.setBorder(new TitledBorder(null, "Basic Component", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		basicCompoPanel.setBackground(Color.WHITE);
-		basicCompoPanel.setBounds(10, 10, 160, 182);
+		basicCompoPanel.setBounds(10, 10, 160, 171);
 		operationPanel.add(basicCompoPanel);
 		basicCompoPanel.setLayout(null);
 
@@ -178,7 +171,7 @@ public class MainFrame extends JFrame {
 		btnTrian.setContentAreaFilled(false);
 		btnTrian.setOpaque(false);
 		btnTrian.setBackground(Color.WHITE);
-		btnTrian.setBounds(28, 32, 40, 40);
+		btnTrian.setBounds(28, 20, 40, 40);
 		btnTrian.setIcon(ImageUtils.getImage("circle.png", 30, 30));
 		basicCompoPanel.add(btnTrian);
 
@@ -187,7 +180,7 @@ public class MainFrame extends JFrame {
 		button.setContentAreaFilled(false);
 		button.setOpaque(false);
 		button.setBackground(Color.WHITE);
-		button.setBounds(92, 32, 40, 40);
+		button.setBounds(92, 20, 40, 40);
 		button.setIcon(ImageUtils.getImage("triangle.png", 30, 30));
 		basicCompoPanel.add(button);
 
@@ -196,7 +189,7 @@ public class MainFrame extends JFrame {
 		button_1.setContentAreaFilled(false);
 		button_1.setOpaque(false);
 		button_1.setBackground(Color.WHITE);
-		button_1.setBounds(92, 82, 40, 40);
+		button_1.setBounds(92, 70, 40, 40);
 		button_1.setIcon(ImageUtils.getImage("trapezoid.png", 30, 30));
 		basicCompoPanel.add(button_1);
 
@@ -205,7 +198,7 @@ public class MainFrame extends JFrame {
 		button_2.setContentAreaFilled(false);
 		button_2.setOpaque(false);
 		button_2.setBackground(Color.WHITE);
-		button_2.setBounds(28, 82, 40, 40);
+		button_2.setBounds(28, 70, 40, 40);
 		button_2.setIcon(ImageUtils.getImage("square.png", 30, 30));
 		basicCompoPanel.add(button_2);
 
@@ -214,7 +207,7 @@ public class MainFrame extends JFrame {
 		button_7.setContentAreaFilled(false);
 		button_7.setOpaque(false);
 		button_7.setBackground(Color.WHITE);
-		button_7.setBounds(28, 132, 40, 40);
+		button_7.setBounds(28, 120, 40, 40);
 		button_7.setIcon(ImageUtils.getImage("ball.png", 30, 30));
 		basicCompoPanel.add(button_7);
 
@@ -223,14 +216,14 @@ public class MainFrame extends JFrame {
 		button_8.setContentAreaFilled(false);
 		button_8.setOpaque(false);
 		button_8.setBackground(Color.WHITE);
-		button_8.setBounds(92, 132, 40, 40);
+		button_8.setBounds(92, 120, 40, 40);
 		basicCompoPanel.add(button_8);
 
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Operation", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(10, 395, 160, 205);
+		panel.setBounds(10, 365, 160, 235);
 		operationPanel.add(panel);
 
 		JButton btnRotation = new JButton();
@@ -238,7 +231,7 @@ public class MainFrame extends JFrame {
 		btnRotation.setContentAreaFilled(false);
 		btnRotation.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnRotation.setBackground(Color.WHITE);
-		btnRotation.setBounds(30, 30, 40, 40);
+		btnRotation.setBounds(29, 53, 40, 40);
 		btnRotation.setIcon(ImageUtils.getImage("rotation.png", 30, 30));
 		panel.add(btnRotation);
 
@@ -247,7 +240,7 @@ public class MainFrame extends JFrame {
 		btnDel.setContentAreaFilled(false);
 		btnDel.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnDel.setBackground(Color.WHITE);
-		btnDel.setBounds(90, 30, 40, 40);
+		btnDel.setBounds(89, 53, 40, 40);
 		btnDel.setIcon(ImageUtils.getImage("delete.png", 30, 30));
 		panel.add(btnDel);
 
@@ -256,12 +249,12 @@ public class MainFrame extends JFrame {
 		btnPlay.setContentAreaFilled(false);
 		btnPlay.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnPlay.setBackground(Color.WHITE);
-		btnPlay.setBounds(30, 80, 100, 30);
+		btnPlay.setBounds(29, 103, 100, 30);
 		btnPlay.setIcon(ImageUtils.getImage("start.png", 20, 20));
 		panel.add(btnPlay);
 
 		JButton btnDonate = new JButton("Donate");
-		btnDonate.setBounds(30, 160, 100, 28);
+		btnDonate.setBounds(29, 183, 100, 28);
 		btnDonate.setIcon(ImageUtils.getImage("donate.png", 20, 20));
 		panel.add(btnDonate);
 		btnDonate.addActionListener(new ActionListener() {
@@ -282,8 +275,17 @@ public class MainFrame extends JFrame {
 		btnPause.setContentAreaFilled(false);
 		btnPause.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		btnPause.setBackground(Color.WHITE);
-		btnPause.setBounds(30, 120, 100, 28);
+		btnPause.setBounds(29, 143, 100, 28);
 		btnPause.setIcon(ImageUtils.getImage("pause.png", 20, 20));
 		panel.add(btnPause);
+
+		JSpinner bodySize = new JSpinner();
+		bodySize.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
+		bodySize.setBounds(69, 21, 60, 22);
+		panel.add(bodySize);
+
+		JLabel lblSize = new JLabel("Size:");
+		lblSize.setBounds(29, 24, 40, 15);
+		panel.add(lblSize);
 	}
 }
